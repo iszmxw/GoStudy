298a166475f0e6e6ec6b692678c19ed2e763924a
追梦小窝 <mail@54zm.com>
2019-11-25 21:02:33 +0800

    学习第二个小案列

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -8,14 +8,14 @@ import (
 	"github.com/goinaction/code/chapter2/sample/search"
 )
 
-// init is called prior to main.
+// init在main之前调用
 func init() {
-	// Change the device for logging to stdout.
+	// 将日志输出到标准输出
 	log.SetOutput(os.Stdout)
 }
 
-// main is the entry point for the program.
+// main是整个程序的入口
 func main() {
-	// Perform the search for the specified term.
+	// 使用特定的项做搜索
 	search.Run("president")
 }
